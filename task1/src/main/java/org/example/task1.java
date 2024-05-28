4c05e8edc4273986a967557aff57143deff35f3b
NikitaAvgopulo <lalakania4@mail.ru>
2024-05-28 18:04:15 +0300

    Add files via upload

---
+++ task1/src/main/java/org/example/task1.java
@@ -0,0 +1,14 @@
+package task1;
+
+public class task1 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        System.out.print(1);
+        int cur = (m - 1) % n + 1;
+        while(cur != 1){
+            System.out.print(cur);
+            cur = (cur + m - 2) % n + 1;
+        }
+    }
+}
